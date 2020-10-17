adba8469317bf87244d0015af5815250a8dd5c65
zhuxinqimac <zhuxinqimac@yahoo.com>
2020-10-18 00:41:02 +1100

    collect stats

--- collect_stats.py
+++ collect_stats.py
@@ -8,7 +8,7 @@
 
 # --- File Name: collect_stats.py
 # --- Creation Date: 17-10-2020
-# --- Last Modified: Sun 18 Oct 2020 00:40:11 AEDT
+# --- Last Modified: Sun 18 Oct 2020 00:40:54 AEDT
 # --- Author: Xinqi Zhu
 # .<.<.<.<.<.<.<.<.<.<.<.<.<.<.<.<
 """
@@ -39,8 +39,8 @@ SUPERVISED_ENTRIES = {
 GOOD_THRESH = {
     'collected-mig-mean.csv': 0.15,
     'collected-dci-mean.csv': 0.3,
-    'collected-factor_vae_metric-mean.csv': 0.7,
-    'collected-beta_vae_sklearn-mean.csv': 0.7
+    'collected-factor_vae_metric-mean.csv': 0.75,
+    'collected-beta_vae_sklearn-mean.csv': 0.75
 }
 BRIEF = {
     'collected-tpl-mean.csv': 'TPL',
